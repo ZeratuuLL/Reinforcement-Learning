0082e6b5dde3f553abaa774458f30ded4ad7188d
ZeratuuLL <lfwei@ucdavis.edu>
2018-10-23 17:47:29 -0700

    Add files via upload

---
+++ Navigation/Net.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed,fc_units=[256,128,64]):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.fc1=nn.Linear(state_size,fc_units[0])
+        self.fc2=nn.Linear(fc_units[0],fc_units[1])
+        self.fc3=nn.Linear(fc_units[1],fc_units[2])
+        self.fc4=nn.Linear(fc_units[2],action_size)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x=F.relu(self.fc1(state))
+        x=F.relu(self.fc2(x))
+        x=F.relu(self.fc3(x))
+        x=self.fc4(x)
+        return(x)
+        
+class Dual_QNetwork(nn.Module):
+    """Actor (Policy) Model."""
+
+    def __init__(self, state_size, action_size, seed,fc_units=[256,128,64]):
+        """Initialize parameters and build model.
+        Params
+        ======
+            state_size (int): Dimension of each state
+            action_size (int): Dimension of each action
+            seed (int): Random seed
+        """
+        super(QNetwork, self).__init__()
+        self.seed = torch.manual_seed(seed)
+        "*** YOUR CODE HERE ***"
+        self.fc1=nn.Linear(state_size,fc_units[0])
+        self.fc2=nn.Linear(fc_units[0],fc_units[1])
+        self.fc3=nn.Linear(fc_units[1],fc_units[2])
+        self.fc4=nn.Linear(fc_units[2],action_size)
+        self.fc5=nn.Linear(fc_units[2],1)
+
+    def forward(self, state):
+        """Build a network that maps state -> action values."""
+        x=F.relu(self.fc1(state))
+        x=F.relu(self.fc2(x))
+        x=F.relu(self.fc3(x))
+        x1=self.fc4(x)
+        x2=self.fc5(x)
+        return(x1+x2)
