576ead822532f8c2dc5c0c0d3bbaa41ba0b928ff
Lifeng Wei <lfwei@ucdavis.edu>
2018-11-11 20:46:16 -0800

    Add files via upload

---
+++ Continuous Control/algorithms.py
@@ -0,0 +1,109 @@
+import numpy as np
+from collections import deque
+from Policy_Agents import DDPG_Agent
+import torch
+
+def ddpg(N, env, n_episodes):
+    '''Use DDPG algorithm to solve the environment
+    This function will not learn from the last N timestamps of each episode
+    Here we assume num_agents=1
+    
+    Params
+    ======
+    N (int): N-step Sarsa
+    env: the unity environment
+    n_episodes (int): max number of episodes for training
+    '''
+    
+    #Some hyper parameters
+    LR = 0.01
+    Gamma = 0.995
+    Tau = 0.02
+    Batch_size = 64
+    Epsilon = 0.001
+    
+    #initialize Brain
+    brain_name = env.brain_names[0]
+    brain = env.brains[brain_name]
+    env_info = env.reset(train_mode=True)[brain_name]
+    num_agents = len(env_info.agents)
+    print('Number of agents:', num_agents)
+    action_size = brain.vector_action_space_size
+    print('Size of each action:', action_size)
+    states = env_info.vector_observations
+    state_size = states.shape[1]
+    print('There are {} agent(s). Each observes a state with length: {}'.format(states.shape[0], state_size))
+    print('The state for the first agent looks like:', states[0])
+    
+    #Initizalize Agent
+    agent = DDPG_Agent(state_size, action_size, learning_rate=LR, gamma=Gamma, tau=Tau, \
+                       batch_size=Batch_size, epsilon=Epsilon)     # Create agent 
+    rewards = []                                                   # Record rewards from all episodes
+    window = deque(maxlen=100)
+    discount = Gamma**np.arange(N)
+        
+    for i in range(1, n_episodes+1):
+        
+        t = 0                                                      # Record how many steps has gone in this episode
+        reward_history = np.zeros(N+1)                             # Record rewards history within this episode
+        states_history = np.zeros((N+1, state_size))               # Record states history within this episode
+        actions_history = np.zeros((N+1, action_size))             # Record rewards actions within this episode
+            
+        # reset The environment & save start information
+        env_info = env.reset(train_mode=True)[brain_name]        
+        state = env_info.vector_observations[0]
+        action = agent.act(state)
+        scores = np.zeros(1)
+        states_history[-1,:] = state.copy()
+        actions_history[-1,:] = action.detach().numpy().copy()
+        
+        while True:
+            #Get new information about the environment
+            env_info = env.step(action.detach().numpy())[brain_name]
+            next_state = env_info.vector_observations[0]
+            reward = env_info.rewards[0]
+            done = env_info.local_done[0]
+            #Update reward history
+            reward_history[:-1] = reward_history[1:]
+            reward_history[-1] = reward
+            scores += reward
+            
+            #Check whether to save memory
+            t += 1
+            if t>=N+1: #Have at least N+1 steps now
+                # Save experience into agent's memory
+                temp_rewards = reward_history[:-1]*discount
+                temp_rewards = np.sum(temp_rewards)
+                agent.memory.add(states_history[0,:].copy(), actions_history[0,:].copy(), temp_rewards,\
+                                 next_state, done)
+            
+            if len(agent.memory.memory)>Batch_size:
+                agent.learn(N)
+                
+            #Save new record
+            next_action = agent.act(next_state)
+            states_history[:-1] = states_history[1:]
+            actions_history[:-1] = actions_history[1:]
+            states_history[-1,:] = next_state
+            actions_history[-1,:] = next_action.detach().numpy()
+            action = next_action
+            if done:
+                break
+        
+        rewards.append(np.sum(scores))
+        window.append(np.sum(scores))
+        print('\rEpisode {}\t Score of this episode: {:.4f}\tAverage Score: {:.4f}'.format(i, np.sum(scores), np.mean(window)), end='')
+        if i % 50 ==0:
+            print('\nEpisode {}\tAverage Score: {:.4f}'.format(i, np.mean(window)))
+        if (np.mean(window)>=30) & (i>=100):
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.4f}'.format(i-100, np.mean(window)))
+            torch.save(agent.actor_local.state_dict(),'actor_checkpoint.pth')
+            torch.save(agent.critic_local.state_dict(),'critic_checkpoint.pth')
+            break
+            
+    print('\nEnvironment not solved!\tAverage Score: {:.4f}'.format(np.mean(window)))
+    agent.actor_local.cpu()
+    agent.critic_local.cpu()
+    torch.save(agent.actor_local.state_dict(),'actor_checkpoint.pth')
+    torch.save(agent.critic_local.state_dict(),'critic_checkpoint.pth')
+    return rewards
---
+++ Continuous Control/infrastructure.py
@@ -0,0 +1,126 @@
+import random
+import numpy as np
+from collections import namedtuple, deque
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+BATCH_SIZE=64
+gpu = input('Please assign the gpu number: ')
+device = torch.device("cuda:"+gpu if torch.cuda.is_available() else "cpu")
+print('Using device: {}'.format(device))
+
+class ReplayBuffer:
+    """Fixed-size buffer to store experience tuples."""
+
+    def __init__(self, batch_size, seed, buffer_size=10**4):
+        """Initialize a ReplayBuffer object.
+
+        Params
+        ======
+            #action_size (int): dimension of each action
+            buffer_size (int): maximum size of buffer
+            batch_size (int): size of each training batch
+            seed (int): random seed
+        """
+        #self.action_size = action_size
+        self.memory = deque(maxlen=buffer_size)  
+        self.batch_size = batch_size
+        self.experience = namedtuple("Experience", field_names=["state", "action", "reward", "target_state",\
+                                                                "done"])
+        self.seed = random.seed(seed)
+    
+    def add(self, state, action, reward, target_state, done):
+        """Add a new experience to memory.
+        
+        Params
+        ======
+        state: the 'current' state
+        action: the 'current' action
+        reward: the processed n_step MC reward
+        target_state: the 'target' state which we would apply bootstrap to estimate value
+        target_action: the action taken at target_state, will only be used when use Sarsa to calculate 
+                       advantage function. Necessary for some continuous cases like DDPG
+        done: If the current state and action ends the episode
+        """
+        
+        e = self.experience(state, action, reward, target_state, done)
+        self.memory.append(e)
+    
+    def sample(self):
+        """Randomly sample a batch of experiences from memory
+        
+        """
+        experiences = random.sample(self.memory, self.batch_size)
+        
+        #Extract information from memory unit and return
+        states = torch.from_numpy(np.vstack([e.state for e in experiences if e is not None])).float().to(device)
+        actions = torch.from_numpy(np.vstack([e.action for e in experiences if e is not None])).float().to(device)
+        rewards = torch.from_numpy(np.vstack([e.reward for e in experiences if e is not None])).float().to(device)
+        target_states = torch.from_numpy(np.vstack([e.target_state for e in experiences if e is not None])).float().to(device)
+        dones = torch.from_numpy(np.vstack([e.done for e in experiences if e is not None]).astype(np.uint8)).float().to(device)
+        return (states, actions, rewards, target_states, dones)
+
+    def __len__(self):
+        """Return the current size of internal memory."""
+        return len(self.memory)
+
+class Actor(nn.Module):
+    '''Network structure for agent's actor'''
+    
+    def __init__(self, state_size, action_size, hidden=[64,64,16], policy=True):
+        '''Build Structure
+        
+        Params
+        ======
+        state_size (int): state size of the environment
+        action_size (int): action size of the environment
+        policy (bool): if True, the output will be a softmax layer, otherwise it would be a fully connected layer
+        '''
+        super(Actor, self).__init__()
+        
+        #compares = [8*state_size, 4*state_size, 2*action_size]
+        #hidden = [max(x,y) for x, y in zip(hidden, compares)]
+        dims = [state_size] + hidden
+        self.action_size = action_size
+        self.layers = nn.ModuleList([nn.Linear(dim_in, dim_out) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
+        self.layers.append(nn.Linear(dims[-1], action_size))
+        self.out = nn.Softmax(dim=0)
+        self.policy = policy
+        
+    def forward(self, x):
+        '''Feed forward'''
+        
+        for layer in self.layers:
+            x = F.relu(layer(x))
+        if self.policy:
+            return self.out(x)
+        else:
+            return x
+    
+class Critic(nn.Module):
+    '''Network structure for agent's critic'''
+    def __init__(self, state_size, action_size, hidden=[256,64,16], action=False):
+        '''Build Structure
+        
+        Params
+        ======
+        state_size (int): state size of the environment
+        action_size (int): action size of the environment
+        action (bool): if True, the critic takes action as a part of input and return Q(s,a), otherwise returns V(s)
+        '''
+        super(Critic, self).__init__()
+        
+        dim = state_size + action*action_size
+        compares = [8*dim, 4*dim, 2*action_size]
+        hidden = [max(x,y) for x, y in zip(hidden, compares)]
+        dims = [dim] + hidden
+        self.layers = nn.ModuleList([nn.Linear(dim_in, dim_out) for dim_in, dim_out in zip(dims[:-1], dims[1:])])
+        self.layers.append(nn.Linear(dims[-1], 1))
+    
+    def forward(self, x):
+        '''Feed forward'''
+
+        for layer in self.layers[:-1]:
+            x = F.relu(layer(x))
+        return self.layers[-1](x)
