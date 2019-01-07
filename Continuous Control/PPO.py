e8099ad4b9f6c58a56c49826df9f2b760a01e9b2
Lifeng Wei <lfwei@ucdavis.edu>
2019-01-06 16:02:37 -0800

    Add files via upload

--- Continuous Control/PPO.py
+++ Continuous Control/PPO.py
@@ -1,32 +1,43 @@
+import torch
+import torch.optim as optim
 from unityagents import UnityEnvironment
-from agents import PPO_Agent
-import numpy as np
-import argparse
 
-parser = argparse.ArgumentParser(description='Parameters for Continuous Control (PPO Agent)')
-parser.add_argument('-n', '--n_episode', type=int,help='Input the number of episodes you want')
-parser.add_argument('-p', '--path', type=str,help='the path of environment file' )
-parser.add_argument('-l1', '--learning_time', type=int,help='How many times to learn in each learning behavior' )
-parser.add_argument('-lr1', '--learning_rate1', type=float,help='learning rate for actor' )
-parser.add_argument('-lr2', '--learning_rate2', type=float,help='learning rate for critic' )
-parser.add_argument('-b', '--beta', type=float,help='Beta for entropy loss' )
-parser.add_argument('-s', '--std', type=float,help='Standard deviation for initial noise generator' )
-parser.add_argument('-e', '--eps', type=float,help='epsilon for ratio clip' )
-parser.add_argument('-l2', '--lambd', type=float,help='lambda parameter for GAE' )
-args = parser.parse_args()
+from infrastructures import PPOPolicyNetwork
+from agents import PPOAgent
 
-n_episode = args.n_episode
-path = args.path
-lr1 = args.learning_rate1
-lr2 = args.learning_rate2
-lambd = args.lambd
-learning_time = args.learning_time
-beta = args.beta
-eps = args.eps
-std = args.std
+env = UnityEnvironment(file_name="./envs/Reacher-20/Reacher")
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+env_info = env.reset(train_mode=True)[brain_name]
 
-env = UnityEnvironment(file_name=path)
-
-agent = PPO_Agent(env=env, lr1=lr1, lr2=lr2, lambd=lambd, beta=beta, learning_time=learning_time, std=std, eps=eps)
-
-agent.train(n_episode)
+config = {
+    'environment': {
+        'state_size':  env_info.vector_observations.shape[1],
+        'action_size': brain.vector_action_space_size,
+        'number_of_agents': len(env_info.agents)
+    },
+    'pytorch': {
+        'device': torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    },
+    'hyperparameters': {
+        'discount_rate': 0.99,
+        'tau': 0.95,
+        'gradient_clip': 5,
+        'rollout_length': 2048,
+        'optimization_epochs': 10,
+        'ppo_clip': 0.2,
+        'log_interval': 2048,
+        'max_steps': 1e5,
+        'mini_batch_number': 32,
+        'entropy_coefficent': 0.01,
+        'episode_count': 250,
+        'hidden_size': 512,
+        'adam_learning_rate': 3e-4,
+        'adam_epsilon': 1e-5
+    }
+}
+    
+policy = PPOPolicyNetwork(config)
+optimizier = optim.Adam(policy.parameters(), config['hyperparameters']['adam_learning_rate'], eps=config['hyperparameters']['adam_epsilon'])
+agent = PPOAgent(env, brain_name, policy, optimizier, config)
+agent.train(1500)
