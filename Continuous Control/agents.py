5fb14e982753eeb9157dafdc5bc63a3f36aa1c4d
Lifeng Wei <lfwei@ucdavis.edu>
2019-02-18 22:26:17 -0800

    Update agents.py

--- Continuous Control/agents.py
+++ Continuous Control/agents.py
@@ -162,7 +162,13 @@ class DDPG_Agent:
 #                  PPO Agent                  #
 #                                             #
 #                                             #
-###############################################   
+############################################### 
+
+#######################################################################
+# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
+# Permission given to modify the code as long as you keep this        #
+# declaration at the top                                              #
+#######################################################################
 
 class PPOAgent(object):
     
